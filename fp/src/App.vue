16132ee00e9b2845a8743dba3b82ca29f4d90a3c
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-09-20 10:46:09 +0700

    base

---
+++ fp/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld
+        msg="Welcome to Your Vue.js App"
+        :is-show-title="isShowTitle"
+    />
+    {{message}}
+    <button v-on:click="c=a+b">Summ {{a}} and {{b}}</button>
+    <button @click="doThat('Hello', $event)">Clear</button>
+    {{c}}
+    <Calc />
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc'
+
+export default {
+  name: 'App',
+  components: {
+    Calc,
+    HelloWorld
+  },
+  data: () => ({
+    message: 'Hi! )',
+    isShowTitle: false,
+    c: 0,
+    a: 1,
+    b: 1
+  }),
+  methods: {
+    doThat (str, event) {
+      console.log(str, event)
+      this.c = 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  cursor: pointer;
+}
+</style>
---
+++ fp/src/components/Calc.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="calc">
+    <div class="main">
+      <input v-model="op1" type="number">
+      <input v-model="op2" type="number">
+      = {{result}}
+    </div>
+    <div class="keyboard">
+      <button @click="result = +op1 + +op2">+</button>
+      <button @click="Minus">-</button>
+      <button @click="Multi(op1, op2)">*</button>
+      <button>/</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calc',
+  data () {
+    return {
+      op1: 0,
+      op2: 0,
+      result: 0
+    }
+  },
+  methods: {
+    Minus () {
+      this.result = +this.op1 - +this.op2
+    },
+    Multi (op1, op2) {
+      this.result = +op1 * +op2
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
